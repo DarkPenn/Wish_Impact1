51280d222eebf6cc97ad5c149b023c4d73b5fa2a
Legion <Legion@DESKTOP-VPHEA6L>
2026-02-27 07:08:08 +0700

    ảnh và độ rộng khung

--- app/src/main/java/com/example/wishimpacttest/MainActivity.kt
+++ app/src/main/java/com/example/wishimpacttest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Typeface
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -17,46 +18,58 @@ enum class Rarity(val stars: Int, val colorHex: String) {
     FIVE_STAR(5, "#FFD700")   // Vàng
 }
 
-data class GachaItem(val name: String, val rarity: Rarity)
+data class GachaItem(val name: String, val rarity: Rarity) //khởi tạo 1 loại lớp dùng để lưu trữ dữ liệu
 
 class MainActivity : AppCompatActivity() {
 
     // 2. Khai báo biến giao diện
-    private lateinit var tvLabel: TextView
-    private lateinit var resultContainer: LinearLayout // <--- THAY ĐỔI: Dùng Container thay vì TextView
+    private lateinit var tvLabel: TextView  //lateinit var: dùng để khai báo biến nhưng chưa gán giá trị
+    private lateinit var resultContainer: LinearLayout
     private lateinit var tvHistory: TextView
     private lateinit var btnWish1: Button
     private lateinit var btnWish10: Button
 
     // 3. Database & Logic
-    private val pool3 = listOf("Kiếm Cùi", "Sách Cũ", "Gậy Gỗ", "Cung Tập Sự")
-    private val pool4 = listOf("Amber", "Kaeya", "Lisa", "Barbara", "Bennett")
-    private val pool5 = listOf("Diluc", "Jean", "Keqing", "Mona", "Qiqi")
+    private val pool3 = listOf("Kiếm Cùi", "Sách Cũ", "Gậy Gỗ", "Cung Tập Sự") //khai báo danh sách vật phấm sẽ rơi ra trong 3*
+    private val pool4 = listOf("Amber", "Kaeya", "Lisa", "Barbara", "Bennett") //khai báo danh sách vật phấm sẽ rơi ra trong 4*
+    private val pool5 = listOf("Diluc", "Jean", "Keqing", "Mona", "Qiqi") //khai báo danh sách vật phấm sẽ rơi ra trong 5*
 
-    private var pity5 = 0
-    private var pity4 = 0
+    private var pity5 = 0 //pity luôn bắt đầu từ 0
+    private var pity4 = 0 //pity luôn bắt đầu từ 0
+    private var currentBannerImage: Int = R.drawable.banner1 //dòng dùng để lưu trữ ID của tấm hình banner được chọn
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         showChooseBanner() // Chạy hàm sử lý trang chọn banner khi start
     }
 
-    private fun showChooseBanner() {
-        setContentView(R.layout.choose_banner) // Hiển thị trang chọn banner
+    private fun showChooseBanner() {setContentView(R.layout.choose_banner)
 
-        // 4. Ánh xạ (Kết nối code với XML choose_banner)
-        val bannerIDs=listOf(R.id.Banner1,R.id.Banner2,R.id.Banner3,R.id.Banner4)
+        // Tạo danh sách các cặp (Nút bấm - Hình ảnh tương ứng)
+        val bannerConfig = mapOf(                   //mapof: giúp quản lí nút nào đi với hình nào
+            R.id.Banner1 to R.drawable.banner1,     //kết nối id banner với ảnh banner
+            R.id.Banner2 to R.drawable.banner2,
+            R.id.Banner3 to R.drawable.banner3,
+            R.id.Banner4 to R.drawable.banner4
+        )
 
-        //Lặp từng ID để thiết lập click listener
-        bannerIDs.forEach { id ->
-            findViewById<Button>(id).setOnClickListener {
-                setupMainActivity() // Chạy hàm thiết lập giao diện khi chọn banner
-            } }
+        // Duyệt qua từng cặp để thiết lập sự kiện
+        bannerConfig.forEach { (btnId, resId) ->
+            findViewById<Button>(btnId).setOnClickListener {
+                currentBannerImage = resId // Dòng dùng để lưu lại hình ảnh được chọn
+                setupMainActivity()        // Sau đó mới chuyển sang màn hình chính
+            }
+        }
     }
 
-    private fun setupMainActivity(){
+    private fun setupMainActivity() {
         setContentView(R.layout.activity_main)
 
+        //Tìm ImageView trong activity_main và gán hình ảnh tương ứng
+        val imageView: ImageView = findViewById(R.id.imageView)
+
+        // Đây chính là dòng giúp app hiển thị đúng Banner
+        imageView.setImageResource(currentBannerImage)
 
         tvHistory = findViewById(R.id.tvHistory)
         btnWish1 = findViewById(R.id.btnWish1)
@@ -64,13 +77,14 @@ class MainActivity : AppCompatActivity() {
 
         // 5. Sự kiện bấm nút
         btnWish1.setOnClickListener {
-            val item = pullOne()
+            val item = pullOne()  //Gọi hàm logic để lấy ra 1 món đồ ngẫu nhiên
+            // listOf(item) là biến 1 món đồ đơn lẻ thành 1 danh sách để hàm hiển thị xử
             showResultInBannerLayout(listOf(item))
         }
 
         btnWish10.setOnClickListener {
-            val items = List(10) { pullOne() }
-            showResultInBannerLayout(items)
+            val items = List(10) { pullOne() }  //Gọi hàm logic để lấy ra 10 món đồ ngẫu nhiên
+            showResultInBannerLayout(items)  //Chuyển sang màn hình kết quả để hiển thị toàn bộ danh sách 10 món đồ
         }
     }
 
@@ -85,12 +99,13 @@ class MainActivity : AppCompatActivity() {
         pity4++
         val rate = Random.nextDouble(0.0, 100.0)
 
-        // Logic 5 sao (0.2% hoặc pity 90)
+
+        // Logic 5 sao (0.001% hoặc pity 90)
         if (pity5 >= 150 || rate <= 0.001) {
             pity5 = 0; pity4 = 0
             return GachaItem(pool5.random(), Rarity.FIVE_STAR)
         }
-        // Logic 4 sao (5.1% hoặc pity 10)
+        // Logic 4 sao (5.7% hoặc pity 10)
         if (pity4 >= 10 || rate <= 5.7) {
             pity4 = 0
             return GachaItem(pool4.random(), Rarity.FOUR_STAR)
@@ -99,7 +114,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     // Hàm cập nhật màn hình (LOGIC MỚI CHO THANH NGANG)
-    // MÀN HÌNH 3: HIỂN THỊ KẾT QUẢ QUAY (banner_layout)
+    // MÀN HÌNH 3: Hiển thị kết quả quay (banner_layout)
     private fun showResultInBannerLayout(items: List<GachaItem>) {
         setContentView(R.layout.banner_layout)
 
@@ -107,25 +122,32 @@ class MainActivity : AppCompatActivity() {
         val resultContainer: LinearLayout = findViewById(R.id.resultContainer)
         val btnBack: Button = findViewById(R.id.btnBackFromResultToBanner)
 
+        // 4. Kiểm tra số lượng đồ: Nếu quay > 1 món (quay x10) thì hiện "Kết quả...", còn lại hiện "Bạn nhận được:"
         tvLabel.text = if (items.size > 1) "Kết quả 10 lần quay:" else "Bạn nhận được:"
 
-        // Hiển thị các vật phẩm quay được vào Horizontal View
+        // Hiển thị các vật phẩm quay được vào Horizontal View thông qua vòng lặp
         items.forEach { item ->
             val itemView = TextView(this)
-            val stars = "★".repeat(item.rarity.stars)
-            itemView.text = "${item.name}\n$stars"
-            itemView.setTextColor(Color.parseColor(item.rarity.colorHex))
+            val stars = "★".repeat(item.rarity.stars) //Tạo hình ngôi sao tương ứng với độ hiếm
+            itemView.text = "${item.name}\n$stars" //Hiển thị tên vật phẩm và hình ngôi sao
+            itemView.setTextColor(Color.parseColor(item.rarity.colorHex)) //Lấy mã màu đã định nghĩa ở phần Rarity để tô màu cho món đồ
             itemView.textSize = 16f
             itemView.typeface = Typeface.DEFAULT_BOLD
             itemView.gravity = Gravity.CENTER
             itemView.setBackgroundColor(Color.parseColor("#3E3E55"))
 
+            //Thiết lập kích thước cho ô kết quả: Rộng 215 pixel
             val params = LinearLayout.LayoutParams(215, LinearLayout.LayoutParams.MATCH_PARENT)
+            // Tạo khoảng cách 5 pixel bên trái và phải để các món đồ không dính sát nhau
             params.setMargins(5, 0, 5, 0)
+            // Áp dụng kích thước vừa thiết lập cho thẻ chữ
             itemView.layoutParams = params
 
+            //Trả kết quả về khung (LinearLayout) hiển thi kết quả
             resultContainer.addView(itemView)
         }
+
+        //Cài đặt nút quay lại màn hình quay
         btnBack.setOnClickListener {
             setupMainActivity()
         }
